a5a58ec81349965a4f2b52091bba545597b0c85f
Sangbeom Heo <sangbeomheo@icloud.com>
2024-10-21 22:25:54 +0900

    기능추가: 로그인, 회원가입

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,34 +1,68 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom'
+import api from '../utils/api'
 
 const LoginPage = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+  const [user, setUser] = useState(null)
+  const navigate = useNavigate()
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    try {
+      const response = await api.post('/user/login', { email, password })
+      if (response.status === 200) {
+        setUser(response.data.user)
+        sessionStorage.setItem('token', response.data.token)
+        api.defaults.headers['authorization'] = 'Bearer ' + response.data.token
+        setError('')
+        navigate('/')
+      } else {
+        throw new Error(response.message)
+      }
+    } catch (error) {
+      setError(error.message)
+    }
+  }
+
   return (
-    <div className="display-center">
-      <Form className="login-box">
+    <div className='display-center'>
+      {error && <div className='color-red'>{error}</div>}
+      <Form className='login-box' onSubmit={handleSubmit}>
         <h1>로그인</h1>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className='mb-3' controlId='formBasicEmail'>
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            onChange={e => setEmail(e.target.value)}
+          />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Group className='mb-3' controlId='formBasicPassword'>
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type='password'
+            placeholder='Password'
+            onChange={e => setPassword(e.target.value)}
+          />
         </Form.Group>
-        <div className="button-box">
-          <Button type="submit" className="button-primary">
+        <div className='button-box'>
+          <Button type='submit' className='button-primary'>
             Login
           </Button>
           <span>
-            계정이 없다면? <Link to="/register">회원가입 하기</Link>
+            계정이 없다면? <Link to='/register'>회원가입 하기</Link>
           </span>
         </div>
       </Form>
     </div>
-  );
-};
+  )
+}
 
-export default LoginPage;
+export default LoginPage
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,38 +1,78 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import api from '../utils/api'
+import { useNavigate } from 'react-router-dom'
 
 const RegisterPage = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [error, setError] = useState('')
+  const navigate = useNavigate()
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    try {
+      if (password !== passwordConfirm) {
+        throw new Error('패스워드가 일치하지 않습니다.')
+      }
+      const response = await api.post('/user', { name, email, password })
+      if (response.status === 200) {
+        navigate('/login')
+      } else {
+        throw new Error(response.data.error)
+      }
+    } catch (error) {
+      setError(error.message)
+    }
+  }
+
   return (
-    <div className="display-center">
-      <Form className="login-box">
+    <div className='display-center'>
+      {error && <div className='color-red'>{error}</div>}
+      <Form className='login-box' onSubmit={handleSubmit}>
         <h1>회원가입</h1>
-        <Form.Group className="mb-3" controlId="formName">
+        <Form.Group className='mb-3' controlId='formName'>
           <Form.Label>Name</Form.Label>
-          <Form.Control type="string" placeholder="Name" />
+          <Form.Control type='string' placeholder='Name' onChange={e => setName(e.target.value)} />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className='mb-3' controlId='formBasicEmail'>
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            onChange={e => setEmail(e.target.value)}
+          />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Group className='mb-3' controlId='formBasicPassword'>
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type='password'
+            placeholder='Password'
+            onChange={e => setPassword(e.target.value)}
+          />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Group className='mb-3' controlId='formBasicPassword'>
           <Form.Label>re-enter the password</Form.Label>
-          <Form.Control type="password" placeholder="re-enter the password" />
+          <Form.Control
+            type='password'
+            placeholder='re-enter the password'
+            onChange={e => setPasswordConfirm(e.target.value)}
+          />
         </Form.Group>
 
-        <Button className="button-primary" type="submit">
+        <Button className='button-primary' type='submit'>
           회원가입
         </Button>
       </Form>
     </div>
-  );
-};
+  )
+}
 
-export default RegisterPage;
+export default RegisterPage
