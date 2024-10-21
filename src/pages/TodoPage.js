3a76bf44a2339556d9666b9e68240f9ad91d25cf
Sangbeom Heo <sangbeomheo@icloud.com>
2024-10-22 00:12:37 +0900

    오타 수정

--- src/pages/TodoPage.js
+++ src/pages/TodoPage.js
@@ -1,87 +1,84 @@
-import React, { useEffect, useState } from "react";
-import TodoBoard from "../components/TodoBoard";
-import api from "../utils/api";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
+import React, { useEffect, useState } from 'react'
+import TodoBoard from '../components/TodoBoard'
+import api from '../utils/api'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
 
 const TodoPage = () => {
-  const [todoList, setTodoList] = useState([]);
-  const [todoValue, setTodoValue] = useState("");
+  const [todoList, setTodoList] = useState([])
+  const [todoValue, setTodoValue] = useState('')
 
   const getTasks = async () => {
-    const response = await api.get("/tasks");
-    setTodoList(response.data.data);
-  };
+    const response = await api.get('/tasks')
+    setTodoList(response.data.data)
+  }
   useEffect(() => {
-    getTasks();
-  }, []);
+    getTasks()
+  }, [])
   const addTodo = async () => {
     try {
-      const response = await api.post("/tasks", {
+      const response = await api.post('/tasks', {
         task: todoValue,
-        isComplete: false,
-      });
+        isCompletd: false,
+      })
       if (response.status === 200) {
-        getTasks();
+        getTasks()
       }
-      setTodoValue("");
+      setTodoValue('')
     } catch (error) {
-      console.log("error:", error);
+      console.log('error:', error)
     }
-  };
+  }
 
-  const deleteItem = async (id) => {
+  const deleteItem = async id => {
     try {
-      console.log(id);
-      const response = await api.delete(`/tasks/${id}`);
+      console.log(id)
+      const response = await api.delete(`/tasks/${id}`)
       if (response.status === 200) {
-        getTasks();
+        getTasks()
       }
     } catch (error) {
-      console.log("error", error);
+      console.log('error', error)
     }
-  };
+  }
 
-  const toggleComplete = async (id) => {
+  const toggleComplete = async id => {
+    console.log(id)
     try {
-      const task = todoList.find((item) => item._id === id);
+      const task = todoList.find(item => item._id === id)
       const response = await api.put(`/tasks/${id}`, {
-        isComplete: !task.isComplete,
-      });
+        isCompleted: !task.isCompleted,
+      })
       if (response.status === 200) {
-        getTasks();
+        getTasks()
       }
     } catch (error) {
-      console.log("error", error);
+      console.log('error', error)
     }
-  };
+  }
   return (
     <Container>
-      <Row className="add-item-row">
+      <Row className='add-item-row'>
         <Col xs={12} sm={10}>
           <input
-            type="text"
-            placeholder="할일을 입력하세요"
-            onChange={(event) => setTodoValue(event.target.value)}
-            className="input-box"
+            type='text'
+            placeholder='할일을 입력하세요'
+            onChange={event => setTodoValue(event.target.value)}
+            className='input-box'
             value={todoValue}
           />
         </Col>
         <Col xs={12} sm={2}>
-          <button onClick={addTodo} className="button-add">
+          <button onClick={addTodo} className='button-add'>
             추가
           </button>
         </Col>
       </Row>
 
-      <TodoBoard
-        todoList={todoList}
-        deleteItem={deleteItem}
-        toggleComplete={toggleComplete}
-      />
+      <TodoBoard todoList={todoList} deleteItem={deleteItem} toggleComplete={toggleComplete} />
     </Container>
-  );
-};
+  )
+}
 
-export default TodoPage;
+export default TodoPage
